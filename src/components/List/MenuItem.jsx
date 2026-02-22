0b0e7a0dce71c97fa115acd17b9c4db0d3d1e752
farshad5631 <farshadkalhor88@gmail.com>
2026-02-22 03:54:12 -0700

    For some components Responsive design corrected.

--- src/components/List/MenuItem.jsx
+++ src/components/List/MenuItem.jsx
@@ -1,33 +1,3 @@
-// import { Link } from "react-router-dom";
-
-// const MenuItem = (props) => {
-//   return (
-//     <div className="flex flex-col border-dotted">
-//       <div className="flex mt-2 justify-between">
-//         <Link to={`/food/${props.id}`}>
-//           <h3 className="text-xl font-bold">{props.title}</h3>
-//         </Link>
-
-//         <span className="flex ml-2 justify-end text-red-500 font-bold">
-//           ${props.price}
-//         </span>
-
-//       </div>
-//       <button
-//             onClick={props.onClick}
-//             className="bg-red-500 text-white px-4 py-2 mt-2 w-1/3 mx-auto rounded-3xl hover:bg-red-300"
-//           >
-//             Add to Cart
-//           </button>
-//       <p className="text-start">{props.text}</p>
-
-//       <div className="border-t-2 border-dotted border-gray-300 my-4"></div>
-//     </div>
-//   );
-// };
-
-// export default MenuItem;
-
 import { Link } from "react-router-dom";
 
 const MenuItem = ({ id, title, price, text, onClick }) => {
